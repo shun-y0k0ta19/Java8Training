fd5c26dbdda9995b1e7731ee0575db0d2bfbc955
y0k0ta19 <motoyk@gmail.com>
2015-06-09 15:43:08 +0900

    add tmp class

---
+++ Java8Exercise/src/ch02/ex02_01/hoge.java
@@ -0,0 +1,10 @@
+package ch02.ex02_01;
+
+public class hoge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Java8Exercise/test/ch01/ex01_02/TestFileExplorer.java
@@ -0,0 +1,14 @@
+package ch01.ex01_02;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestFileExplorer {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
