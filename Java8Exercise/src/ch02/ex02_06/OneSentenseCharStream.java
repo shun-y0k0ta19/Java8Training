4c4dbf8304bd120490d7fe8fd4536e9803e8a739
y0k0ta19 <motoyk@gmail.com>
2015-08-31 15:12:17 +0900

    change javadoc

--- Java8Exercise/src/ch02/ex02_06/OneSentenseCharStream.java
+++ Java8Exercise/src/ch02/ex02_06/OneSentenseCharStream.java
@@ -9,7 +9,6 @@ public class OneSentenseCharStream {
 	    return IntStream.range(0, s.length()).mapToObj(n -> s.charAt(n));
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		characterStream("test").forEach(System.out::println);
 	}
 
