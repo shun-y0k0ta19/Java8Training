39944ab5f0299ad80b014505c9dc326a3dea4ee4
y0k0ta19 <motoyk@gmail.com>
2015-09-27 03:46:21 +0900

    fix warning

--- Java8Exercise/src/ch04/ex04_10/BrowserApp.java
+++ Java8Exercise/src/ch04/ex04_10/BrowserApp.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
