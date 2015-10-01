0346a54a4b23e834c55fae0525dea5b1da8f1265
y0k0ta19 <motoyk@gmail.com>
2015-10-01 17:27:11 +0900

    make class (not implement)

---
+++ Java8Exercise/src/ch06/ex05/FileReader.java
@@ -0,0 +1,5 @@
+package ch06.ex05;
+
+public class FileReader {
+
+}
---
+++ Java8Exercise/src/ch06/ex05/WordCollector.java
@@ -0,0 +1,18 @@
+package ch06.ex05;
+
+import java.io.File;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class WordCollector {
+
+	public ConcurrentHashMap<String, Set<File>> collectWordsFromFiles(){
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
