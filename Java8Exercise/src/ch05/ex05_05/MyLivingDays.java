020e5f509416d86be8b6d878b6d643c8351f7a6a
y0k0ta19 <motoyk@gmail.com>
2015-10-01 17:25:54 +0900

    add calculate living day until birthday

--- Java8Exercise/src/ch05/ex05_05/MyLivingDays.java
+++ Java8Exercise/src/ch05/ex05_05/MyLivingDays.java
@@ -6,11 +6,20 @@ import java.time.temporal.ChronoUnit;
 public class MyLivingDays {
 
 	public static void main(String[] args) {
-		LocalDate myBirthDay = LocalDate.of(1988, 9, 5);
+		myLivingDays(1988, 9, 5);
+		calcLivingDays(LocalDate.of(1988, 9, 5), 10000);
+	}
+
+	public static void myLivingDays(int year, int month, int day) {
+		LocalDate myBirthDay = LocalDate.of(year, month, day);
 		LocalDate today = LocalDate.now();
 		long myLivingDays = myBirthDay.until(today, ChronoUnit.DAYS);
 		System.out.println(myLivingDays);
 		System.out.println(LocalDate.of(1988, 10, 12).plusDays(10000));
 	}
-
+	
+	public static void calcLivingDays(LocalDate birthday, long calcLivingDays){
+		System.out.println(birthday.plusDays(calcLivingDays));
+	}
+	
 }
