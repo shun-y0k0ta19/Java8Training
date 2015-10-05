b1c859cedcd198871a049210295328fa067ea8b1
y0k0ta19 <motoyk@gmail.com>
2015-10-05 14:10:14 +0900

    done impl

--- Java8Exercise/src/ch06/ex05/WordCollector.java
+++ Java8Exercise/src/ch06/ex05/WordCollector.java
@@ -1,17 +1,43 @@
 package ch06.ex05;
 
+import static MyUtilities.FileUtils.*;
+
 import java.io.File;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class WordCollector {
 
-	public ConcurrentHashMap<String, Set<File>> collectWordsFromFiles(){
-		return null;
+	public ConcurrentHashMap<String, Set<File>> collectWordsFromFiles(Set<File> files){
+		ConcurrentHashMap<String, Set<File>> filesContainingWord = new ConcurrentHashMap<>();
+		files.stream().parallel().forEach(file -> {
+			//merge用のfileのセットを作る
+			Set<File> fileSet = new HashSet<>();
+			fileSet.add(file);
+			
+			Set<String> wordSet = createWordSet(file.toPath());
+			//wordSet.stream().parallel().forEach(word ->{
+			wordSet.stream().forEach(word -> {
+				filesContainingWord.merge(word, fileSet, (existSet, newSet) -> {
+					existSet.addAll(newSet);
+					return existSet;
+				});
+			});
+		});
+		return filesContainingWord;
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WordCollector wc = new WordCollector();
+		HashSet<File> files = new HashSet<>();
+		files.add(new File("src/files/war-and-peace.txt"));
+		files.add(new File("src/files/alice.txt"));
+		
+		long start = System.nanoTime();
+		wc.collectWordsFromFiles(files);
+		long end = System.nanoTime();
+		System.out.print(end - start);
 
 	}
 
