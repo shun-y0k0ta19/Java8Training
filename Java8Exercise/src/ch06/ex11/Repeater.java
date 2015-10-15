8f5bbcd71127b28b28b40e483819b255bf519f85
y0k0ta19 <motoyk@gmail.com>
2015-10-16 08:38:56 +0900

    commit

--- Java8Exercise/src/ch06/ex11/Repeater.java
+++ Java8Exercise/src/ch06/ex11/Repeater.java
@@ -1,28 +1,63 @@
 package ch06.ex11;
 
+import java.net.PasswordAuthentication;
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class Repeater {
 
 	public static <T> CompletableFuture<T> repeat(Supplier<T> action, Predicate<T> until) {
-		return null;
+		return CompletableFuture.supplyAsync(action).thenComposeAsync(t -> {
+			if(until.test(t)) {
+				return CompletableFuture.completedFuture(t);
+			} else {
+				return repeat(action, until);
+			}
+		});
 	}
-	
-	private static <T> T doAsync(Supplier<T> action, Predicate<T> until) {
-		T result = action.get();
-		if(until.test(result)) {
-			return result;
-		}
-		return doAsync(action, until);
-	}
-	
+
+
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		PasswordAuthentication pa = new PasswordAuthentication("yokota", "secret".toCharArray());
 
+		Supplier<String> action = () -> {
+			Scanner scanner = new Scanner(System.in);
+			String pass = "";
+			for(;;){
+				if(scanner.hasNextLine()) {
+					pass = scanner.nextLine();
+					break;
+				}
+			}
+			scanner.close();
+			return pass;
+		};
+
+		Predicate<String> until = pass -> {
+			System.out.println(Arrays.equals(pa.getPassword(), pass.toCharArray()));
+			return (Arrays.equals(pa.getPassword(), pass.toCharArray()));
+		};
+		CompletableFuture<String> result = repeat(action, until);
+		for(;;) {
+			try {
+				System.out.println("match!" + result.get(1, TimeUnit.SECONDS));
+				break;
+			} catch (ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (TimeoutException e) {
+				
+			}catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
-
 }
